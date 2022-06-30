0907d3a01445b56400fda4959a205e234ceccd21
Sofia <mariasofia.muller@gmail.com>
2022-06-29 21:31:10 -0400

    Arrays

---
+++ index.js
@@ -0,0 +1,28 @@
+function sumarCuotas(cuotaUno, cuotaDos, cuotaTres, cuotaCuatro) {
+    let suma = (cuotaUno + cuotaDos + cuotaTres + cuotaCuatro)*1.3
+    alert("La suma de sus cuotas es "+ suma)
+}
+
+let cuotaUno = parseInt(prompt("Ingrese su primera cuota (recuerde que a cada cuota se le adiciona un 3% de interes)"))
+let cuotaDos = parseInt(prompt("Ingrese su segunda cuota (recuerde que a cada cuota se le adiciona un 3% de interes)"))
+let cuotaTres = parseInt(prompt("Ingrese su tercera cuota (recuerde que a cada cuota se le adiciona un 3% de interes)"))
+let cuotaCuatro = parseInt(prompt("Ingrese su cuarta cuota (recuerde que a cada cuota se le adiciona un 3% de interes)"))
+
+sumarCuotas(cuotaUno, cuotaDos, cuotaTres, cuotaCuatro)
+
+
+
+const productos = [
+    {nombre: 'Television', precio: 2500},
+    {nombre: 'Escritorio', precio: 12500},
+]
+
+const resultado = productos.find((producto) => producto.nombre === "Television")
+
+
+const resultadDos = productos.find((producto) => producto.nombre === "Espejo")
+
+
+console.log(resultado)
+console.log(resultadDos)
+
